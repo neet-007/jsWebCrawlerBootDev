4fee394c527ddb25a6dd1f7b0becb5b877fe114b
neet-007 <moayedeffect@gmail.com>
2024-08-10 01:20:29 +0300

    added get url from html function and test

--- crawler.js
+++ crawler.js
@@ -1,3 +1,26 @@
+const { JSDOM } = require('jsdom');
+
+function getUrlsFromHTML(html, baseUrl) {
+	const urlArr = [];
+	const htmlObj = new JSDOM(html);
+	const links = htmlObj.window.document.querySelectorAll("a");
+
+	for (let i = 0; i < links.length; i++) {
+		let url = links[i].href;
+		if (url.startsWith("/")) {
+			url = `${baseUrl}${url}`;
+		}
+		try {
+			const urlObj = new URL(url);
+			urlArr.push(urlObj.href);
+		} catch (err) {
+			console.log(err.message);
+		}
+	}
+
+	return urlArr
+}
+
 function normalizeUrl(url) {
 	const urlObj = new URL(url);
 	const return_val = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,5 +32,6 @@ function normalizeUrl(url) {
 }
 
 module.exports = {
-	normalizeUrl
+	normalizeUrl,
+	getUrlsFromHTML
 }
--- crawler.test.js
+++ crawler.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require("./crawler.js");
+const { normalizeUrl, getUrlsFromHTML } = require("./crawler.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeUrl strip hostname path", () => {
@@ -31,3 +31,61 @@ test("normalizeUrl capital", () => {
 	const expected = "example/path";
 	expect(actual).toEqual(expected);
 })
+
+
+test("getUrlFromHTML full", () => {
+	const input1 = `
+<html>
+	<body>
+		<a href="https://example.com/path">
+			path html 1
+		</a>
+	</body>
+</html>
+
+`;
+	const input2 = "https://example.com/";
+	const actual = getUrlsFromHTML(input1, input2);
+	const expected = ["https://example.com/path"];
+	expect(actual).toEqual(expected);
+})
+
+
+test("getUrlFromHTML relatev", () => {
+	const input1 = `
+<html>
+	<body>
+		<a href="/path">
+			path html 1
+		</a>
+	</body>
+</html>
+
+`;
+	const input2 = "https://example.com";
+	const actual = getUrlsFromHTML(input1, input2);
+	const expected = ["https://example.com/path"];
+	expect(actual).toEqual(expected);
+})
+
+
+test("getUrlFromHTML both", () => {
+	const input1 = `
+<html>
+	<body>
+		<a href="https://example.com/path1">
+			path html 1
+		</a>
+		<a href="/path2">
+
+		</a>
+	</body>
+</html>
+
+`;
+	const input2 = "https://example.com"
+	const actual = getUrlsFromHTML(input1, input2);
+	const expected = ["https://example.com/path1", "https://example.com/path2"];
+	expect(actual).toEqual(expected);
+})
+
